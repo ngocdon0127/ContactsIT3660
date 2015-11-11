8698ba5e163b3c7ccfa115ac35e5962ae4e48e3b
Ngoc Don <kaitoukid2701@gmail.com>
2015-11-12 02:16:27 +0700

    Add Restore-Contacts Feature

---
+++ app/src/main/java/vn/hoasinhvien/contactsit3660/SharedData.java
@@ -0,0 +1,50 @@
+package vn.hoasinhvien.contactsit3660;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Handler;
+
+/**
+ * Created by NgocDon on 11/12/2015.
+ */
+public class SharedData {
+
+    public static Handler handler = new Handler();
+    public static ProgressDialog dialog;
+    public static int progress = 0;
+
+    public static void addProgressDialog(String s, Context context){
+        progress = 0;
+        dialog = new ProgressDialog(context);
+        dialog.setMax(1);
+        dialog.setMessage(s);
+        dialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        dialog.setProgress(progress);
+        dialog.setCancelable(false);
+        dialog.show();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    while (true){
+                        Thread.sleep(100);
+                        handler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                dialog.setProgress(progress);
+                            }
+                        });
+                        if (progress == 1)
+                            break;
+                    }
+                    dialog.dismiss();
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+}
